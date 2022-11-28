0daccf5b3d9b48b836bc9dd725b80450e7d406ac
MCBurgerHD <117370682+MCBurgerHD@users.noreply.github.com>
2022-11-28 15:26:09 +0100

    Delete Message directory

--- Message/src/app/warning/warning.component.ts
+++
@@ -1,9 +0,0 @@
-import{ Component} from'@angular/core';
-@Component({
-  selector: 'app-warning',
-  template: 'Warning! Something is wrong!',
-  styles:[':host{border: 1px solid white;background: orange;text-align: center;width: 25%;border-radius: 3px;}']
-})
-export class WarningComponent{
-  constructor() {}
-}
